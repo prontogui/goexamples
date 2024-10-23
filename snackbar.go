486316d1c60dee6076ccdf5af6665df569e7c210
Andy Joseph <ahjoseph@me.com>
2024-10-23 10:57:29 -0400

    Added snackbar case where button is shown.

--- snackbar.go
+++ snackbar.go
@@ -13,21 +13,38 @@ func snackbarDemo(pgui pg.ProntoGUI) {
 		Embodiment: "{\"fontSize\":\"25.0\"}",
 	}.Make()
 
-	cmd := pg.NewCommand("Show Snackbar")
+	cmd1 := pg.NewCommand("Show snackbar with text")
+	cmd2 := pg.NewCommand("Show snackbar with text + button")
+	msg := pg.NewText("")
 
-	snackbar := pg.NewFrame(pg.NewText("This is an important message!")).SetEmbodiment("snackbar")
+	snackbar1 := pg.NewFrame(pg.NewText("This is an important message!")).SetEmbodiment("snackbar")
 
-	pgui.SetGUI(heading, cmd, snackbar)
+	detailsCmd := pg.NewCommand("Details")
+	snackbar2 := pg.NewFrame(pg.NewText("Something didn't go as planned"), detailsCmd).SetEmbodiment("snackbar")
+
+	pgui.SetGUI(heading, cmd1, cmd2, msg, snackbar1, snackbar2)
+
+	detailClicks := 0
 
 	for {
+
 		_, err := pgui.Wait()
 		if err != nil {
 			fmt.Printf("error from Wait() is:  %s\n", err.Error())
 			break
 		}
 
-		if cmd.Issued() {
-			snackbar.SetShowing(true)
+		if cmd1.Issued() {
+			snackbar1.SetShowing(true)
+		}
+
+		if cmd2.Issued() {
+			snackbar2.SetShowing(true)
+		}
+
+		if detailsCmd.Issued() {
+			detailClicks = detailClicks + 1
+			msg.SetContent(fmt.Sprintf("Details clicked %d times.", detailClicks))
 		}
 	}
 }
