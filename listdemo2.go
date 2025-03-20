30e9df66ae813b525dfd13e326351a37fcdb8b0e
Devin Smith <devs@prontogui.com>
2025-03-20 14:20:03 -0400

    updated to latest golib version 0.0.56

--- listdemo2.go
+++ listdemo2.go
@@ -8,36 +8,39 @@ import (
 
 func listDemo2(pgui pg.ProntoGUI) {
 
-	item1 := pg.GroupWith{GroupItems: []pg.Primitive{
-		pg.CheckWith{}.Make(),
-		pg.TextWith{Content: "Wheel"}.Make(),
-		pg.TextWith{Content: "6 with rubber tread"}.Make(),
-		pg.CommandWith{Label: "...", Embodiment: "outlined-button"}.Make(),
-	}}.Make()
-	item2 := pg.GroupWith{GroupItems: []pg.Primitive{
-		pg.CheckWith{}.Make(),
-		pg.TextWith{Content: "Brake"}.Make(),
-		pg.TextWith{Content: "Carbide polymer"}.Make(),
-		pg.CommandWith{Label: "..."}.Make(),
-	}}.Make()
-	item3 := pg.GroupWith{GroupItems: []pg.Primitive{
-		pg.CheckWith{}.Make(),
-		pg.TextWith{Content: "Engine"}.Make(),
-		pg.TextWith{Content: "4 cyclinder gasoline"}.Make(),
-		pg.CommandWith{Label: "..."}.Make(),
-	}}.Make()
-	item4 := pg.GroupWith{GroupItems: []pg.Primitive{
-		pg.CheckWith{}.Make(),
-		pg.TextWith{Content: "Transmission"}.Make(),
-		pg.TextWith{Content: "3 speed manual"}.Make(),
-		pg.CommandWith{Label: "..."}.Make(),
-	}}.Make()
-	list := pg.ListWith{
-		ListItems:  []pg.Primitive{item1, item2, item3, item4},
-		Embodiment: "card-list",
+	item1 := pg.CardWith{
+		LeadingItem:  pg.CheckWith{}.Make(),
+		MainItem:     pg.TextWith{Content: "Wheel"}.Make(),
+		SubItem:      pg.TextWith{Content: "6 with rubber tread"}.Make(),
+		TrailingItem: pg.CommandWith{Label: "..."}.Make(),
 	}.Make()
+
+	item2 := pg.CardWith{
+		LeadingItem:  pg.CheckWith{}.Make(),
+		MainItem:     pg.TextWith{Content: "Brake"}.Make(),
+		SubItem:      pg.TextWith{Content: "Carbide polymer"}.Make(),
+		TrailingItem: pg.CommandWith{Label: "..."}.Make(),
+	}.Make()
+
+	item3 := pg.CardWith{
+		LeadingItem:  pg.CheckWith{}.Make(),
+		MainItem:     pg.TextWith{Content: "Engine"}.Make(),
+		SubItem:      pg.TextWith{Content: "4 cyclinder gasoline"}.Make(),
+		TrailingItem: pg.CommandWith{Label: "..."}.Make(),
+	}.Make()
+
+	item4 := pg.CardWith{
+		LeadingItem:  pg.CheckWith{}.Make(),
+		MainItem:     pg.TextWith{Content: "Transmission"}.Make(),
+		SubItem:      pg.TextWith{Content: "3 speed manual"}.Make(),
+		TrailingItem: pg.CommandWith{Label: "..."}.Make(),
+	}.Make()
+
+	list := pg.ListWith{
+		ListItems: []pg.Primitive{item1, item2, item3, item4},
+	}.Make()
+
 	pgui.SetGUI(list)
-	pgui.Wait()
 
 	for {
 		_, err := pgui.Wait()
